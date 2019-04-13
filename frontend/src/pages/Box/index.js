2baee29f26f5069c5b0138c30ea9cdae882bede1
gustavoaz7 <gustavo_azevedo_7@hotmail.com>
2019-04-13 16:34:16 -0300

    Render user Box

--- frontend/src/pages/Box/index.js
+++ frontend/src/pages/Box/index.js
@@ -1,7 +1,45 @@
 import React, { Component } from "react";
+import { MdInsertDriveFile } from "react-icons/md";
+import { distanceInWords } from "date-fns";
+import logo from "../../assets/logo.png";
+import "./styles.css";
+import API from "../../services/API";
 
 export default class Box extends Component {
+  state = {
+    box: {}
+  };
+
+  componentDidMount = async () => {
+    const box = this.props.match.params.id;
+    const response = await API.get(`boxes/${box}`);
+    this.setState({ box: response.data });
+  };
+
   render() {
-    return null;
+    const { box } = this.state;
+    return (
+      <div id="box-container">
+        <header>
+          <img src={logo} alt="" />
+          <h1>{box.title}</h1>
+        </header>
+
+        <ul>
+          {box.files &&
+            box.files.map(file => (
+              <li>
+                <a className="fileInfo" href={file.url} target="_blank">
+                  <MdInsertDriveFile size={24} color="#0070e0" />
+                  <strong>{file.title}</strong>
+                </a>
+                <span>
+                  {distanceInWords(file.createdAt, new Date()) + " ago"}
+                </span>
+              </li>
+            ))}
+        </ul>
+      </div>
+    );
   }
 }
